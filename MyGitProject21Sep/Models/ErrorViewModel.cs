274417422ab5ac3231f576b80682f5377823e813
Mohammad Tahir <md.tahir1@gmail.com>
2022-09-21 09:07:06 +0530

    Committed

---
+++ MyGitProject21Sep/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyGitProject21Sep.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
